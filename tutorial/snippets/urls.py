f72cb8e196a09eb6f5de6989ad8129129fce240e
THASNEEMMOOOSA <60733459+THASNEEMMOOOSA@users.noreply.github.com>
2024-01-24 10:33:42 +0530

    Add files via upload

---
+++ tutorial/snippets/urls.py
@@ -0,0 +1,40 @@
+from rest_framework import renderers
+from django.urls import path,include
+from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
+from snippets import views
+
+from snippets.views import  SnippetViewSet, UserViewSet
+
+snippet_list = SnippetViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+snippet_detail = SnippetViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+snippet_highlight = SnippetViewSet.as_view({
+    'get': 'highlight'
+}, renderer_classes=[renderers.StaticHTMLRenderer])
+user_list = UserViewSet.as_view({
+    'get': 'list'
+})
+user_detail = UserViewSet.as_view({
+    'get': 'retrieve'
+})
+
+
+
+
+
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet, basename='snippet')
+router.register(r'users', views.UserViewSet, basename='user')
+
+# The API URLs are now determined automatically by the router.
+urlpatterns = [
+    path('', include(router.urls)),
+]
